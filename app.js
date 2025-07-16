61f591b0d714db4671a4161c3ff1db56500f34fc
citronoiseau <avroraka2004@gmail.com>
2025-07-16 08:38:18 +0000

    added multer to upload files

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use("/uploads", express.static("uploads"));
 
 app.use(
   session({
